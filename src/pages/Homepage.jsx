853944c3d74267d514762a474fbf9c561831d3ae
Hannan1717 <hannanelang17@gmail.com>
2024-01-01 21:35:30 +0700

    useScrollAnchor dan useModalDOM

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import Header from "../parts/Header";
-import Hero from "../parts/Hero";
+import Hero from "../parts/Homepage/Hero";
 import JustArrived from "../parts/Homepage/JustArrived";
 import BrowseRoom from "../parts/Homepage/BrowseRoom";
 import Clients from "../parts/Clients";
 import SiteMap from "../parts/Homepage/SiteMap";
 import Footer from "../parts/Footer";
+import useScrollAnchor from "../helpers/hooks/useScrollAnchor";
+import useModalDOM from "../helpers/hooks/useModalDOM";
+
 export default function Homepage() {
+    useScrollAnchor();
+    useModalDOM();
     return (
         <>
             <Header theme={"white"} position={"absolute"} />
